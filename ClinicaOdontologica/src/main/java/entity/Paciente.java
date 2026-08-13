84ae1db784c220926ac83a6f31bbe1b31a8f961b
Júlia <@julia-py>
2026-08-12 21:48:15 -0300

    feat:adiciona estrutura inicial do projeto

---
+++ ClinicaOdontologica/src/main/java/entity/Paciente.java
@@ -0,0 +1,16 @@
+package entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+
+public class Paciente {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String nome;
+    private String cpf;
+}
